79379be404f5c4941c3d1ceb2fcb7fe59688f752
Joel Campos <joelcamposoliva9000@gmail.com>
2021-05-02 19:30:20 +0200

    Update firebase_config.js

--- public/js/firebase_config.js
+++ public/js/firebase_config.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration (you'll need to replace this with yours)
 var firebaseConfig = {
-    apiKey: "AIzaSyAnZW0V4wTzTZJS9pVxfkJS32nzkLGYZQg",
-    authDomain: "basketballmatchscore.firebaseapp.com",
-    projectId: "basketballmatchscore",
-    storageBucket: "basketballmatchscore.appspot.com",
-    messagingSenderId: "598755930117",
-    appId: "1:598755930117:web:4ca91b0537d4d90c246c6c"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
